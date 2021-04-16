8b38af0fe7bf4288edad209116204e64779a7b9e
堂铃 <5516023@qq.com>
2021-04-16 10:40:29 +0800

    ✨   新增自启浏览器

--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
-const express = require("express");
-const app = express();
-const path = require("path");
+const express = require("express")
+const chalk = require("chalk")
+const app = express()
+const path = require("path")
 
-const json = require("../build/json");
+const json = require("../build/json")
 
 // main route
-app.get("/", (req, res) => res.sendFile(path.resolve(__dirname, "./index.html")));
-app.get("/json", (req, res) => res.send(json));
+app.get("/", (req, res) => res.sendFile(path.resolve(__dirname, "./index.html")))
+app.get("/json", (req, res) => res.send(json))
 
 // app start up
-app.listen(7707, () => console.log("Express server listening on port: http://localhost:7707"));
+app.listen(7707, () => {
+  process.stdout.write(process.platform === "win32" ? "\x1B[2J\x1B[0f" : "\x1B[2J\x1B[3J\x1B[H")
+  console.log('\x1b[34m%s\x1b[0m  \x1b[33m%s\x1b[0m','QiEditor Dev Server:', 'http://localhost:7707');
+})
